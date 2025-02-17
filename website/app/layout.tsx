d6f88c6d167586c01f6528c3a65adacf62d84c82
Aayla Secura <aayla.secura.1138@gmail.com>
2025-02-17 17:36:11 +0200

    Optimized SEO (open graph meta)

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -13,6 +13,22 @@ export const metadata: Metadata = {
   description:
     "Lightweight and simple. Powerful and flexible. LISN handles all complexities so you can simply handle user gestures, actions and events.",
   keywords: lisnPkgJson.keywords,
+  openGraph: {
+    title: "LISN.js",
+    description:
+      "Lightweight and simple. Powerful and flexible. LISN handles all complexities so you can simply handle user gestures, actions and events.",
+    url: "https://lisnjs.github.io/",
+    siteName: "LISN.js",
+    images: [
+      {
+        url: "https://lisnjs.github.io/cover.jpg",
+        width: 1200,
+        height: 600,
+        alt: "LISN.js preview image",
+      },
+    ],
+    type: "website",
+  },
 };
 
 const roboto = Roboto({

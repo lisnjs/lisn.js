37739c306605472bf5d15ea540e50f77e7d5c02c
Aayla Secura <aayla.secura.1138@gmail.com>
2025-02-23 17:13:11 +0200

    Reordered doc sidebar modules

--- packages/lisn.js/src/ts/index.ts
+++ packages/lisn.js/src/ts/index.ts
@@ -4,9 +4,9 @@
  * @internal
  */
 
-export * from "@lisn/globals/index";
 export * from "@lisn/actions/index";
 export * from "@lisn/debug/index";
+export * from "@lisn/globals/index";
 export * from "@lisn/modules/index";
 export * from "@lisn/triggers/index";
 export * from "@lisn/utils/index";
